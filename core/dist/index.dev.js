b681345d66e6a9af4d465c70adfa06faaffbb5f2
sieugene <evgenii.sidelnikov@unitbean.io>
2020-11-22 21:20:38 +0300

    basic logic

--- core/dist/index.dev.js
+++ core/dist/index.dev.js
@@ -18,7 +18,10 @@ function () {
     _classCallCheck(this, Core);
 
     this.$root = document.querySelector(selector);
-  }
+    this.inputs = [];
+    this.$input = null;
+  } //DOM
+
 
   _createClass(Core, [{
     key: "find",
@@ -26,12 +29,107 @@ function () {
       var $el = this.$root.querySelector(selector);
       return $el;
     }
+  }, {
+    key: "findAll",
+    value: function findAll(selector) {
+      var $el = this.$root.querySelectorAll(selector);
+      return $el;
+    }
+  }, {
+    key: "toHtml",
+    value: function toHtml(el, template) {
+      var $el = this.find(el);
+      $el.innerHTML = ($el.innerHTML + template).trim();
+    } //Input
+
+  }, {
+    key: "setInput",
+    value: function setInput($el) {
+      this.$input = $el;
+      $el.addEventListener("input", inputChange);
+      $el.addEventListener("keydown", pressKey);
+    }
+  }, {
+    key: "savePrevText",
+    value: function savePrevText($el) {
+      var input = this.find("[data-input=\"".concat($el.dataset.input, "\"]"));
+      input.outerHTML = "<input \n                      type=\"text\" \n                      id=\"command\" \n                      data-input=\"".concat(input.dataset.input, "\" \n                      value=\"").concat($el.value, "\"\n                      disabled=\"true\">\n                      </input>");
+      input.value = $el.value;
+    }
+  }, {
+    key: "disableInput",
+    value: function disableInput($el) {
+      var id = Number($el.dataset.input) + 1;
+      $el.removeEventListener("input", inputChange);
+      $el.removeEventListener("keydown", pressKey);
+      this.$input = null;
+      this.addInput(id);
+      this.savePrevText($el);
+    }
+  }, {
+    key: "addInput",
+    value: function addInput(id) {
+      this.toHtml(".terminal-content", InputTemplate(id));
+      this.focusOnLast();
+      this.disableInputs();
+    } //Inputs
+
+  }, {
+    key: "setInputs",
+    value: function setInputs(inputs) {
+      this.inputs = inputs;
+    }
+  }, {
+    key: "disableInputs",
+    value: function disableInputs() {
+      if (this.inputs && this.inputs.length) {
+        var LAST = this.inputs.length - 1;
+        this.inputs.forEach(function (element, idx) {
+          if (LAST !== idx) {
+            element.disabled = true;
+          }
+        });
+      }
+    }
+  }, {
+    key: "focusOnLast",
+    value: function focusOnLast() {
+      var inputs = this.findAll("#command");
+      root.setInputs(inputs);
+      var last = inputs[inputs.length - 1];
+      last.focus();
+      this.setInput(last);
+    } //Terminal
+
+  }, {
+    key: "terminalEventClick",
+    value: function terminalEventClick() {
+      var _this = this;
+
+      var terminal = this.find(".terminal");
+      terminal.addEventListener("click", function (event) {
+        var target = event.target;
+
+        if (target.className === "terminal") {
+          _this.focusOnLast.bind(_this)();
+        }
+      });
+    } //Core
+
+  }, {
+    key: "init",
+    value: function init() {
+      this.focusOnLast();
+      this.terminalEventClick();
+      this.disableInputs();
+    }
   }]);
 
   return Core;
 }();
 
 var root = new Core("#root");
+root.init();
 
 var timer = function timer() {
   var timerEl = root.find("#timer");
@@ -43,4 +141,26 @@ var timer = function timer() {
   }, 1000);
 };
 
-timer();
+timer(); //event Handlers
+
+function inputChange(event) {}
+
+function pressKey(event) {
+  var keys = ["Enter", "Tab"];
+
+  if (keys.includes(event.key)) {
+    event.preventDefault();
+    root.disableInput(root.$input);
+  }
+}
+
+var commandInputs = function commandInputs() {
+  var input = root.find("[data-input='0']"); // debugger;
+}; // commandInputs();
+// Templates
+
+
+var InputTemplate = function InputTemplate(id) {
+  var value = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "";
+  return "<div class=\"terminal-input\">\n  <div class=\"path cyan\">admin > </div>\n  <input type=\"text\" id=\"command\" data-input=\"".concat(id, "\" value=\"").concat(value, "\">\n</div>");
+};
--- core/index.js
+++ core/index.js
@@ -6,14 +6,93 @@ const FormatTime = (date) => {
 class Core {
   constructor(selector) {
     this.$root = document.querySelector(selector);
+    this.inputs = [];
+    this.$input = null;
   }
+  //DOM
   find(selector) {
     const $el = this.$root.querySelector(selector);
     return $el;
   }
+  findAll(selector) {
+    const $el = this.$root.querySelectorAll(selector);
+    return $el;
+  }
+  toHtml(el, template) {
+    let $el = this.find(el);
+    $el.innerHTML = ($el.innerHTML + template).trim();
+  }
+  //Input
+  setInput($el) {
+    this.$input = $el;
+    $el.addEventListener("input", inputChange);
+    $el.addEventListener("keydown", pressKey);
+  }
+  savePrevText($el) {
+    let input = this.find(`[data-input="${$el.dataset.input}"]`);
+    input.outerHTML = `<input 
+                      type="text" 
+                      id="command" 
+                      data-input="${input.dataset.input}" 
+                      value="${$el.value}"
+                      disabled="true">
+                      </input>`;
+    input.value = $el.value;
+  }
+  disableInput($el) {
+    const id = Number($el.dataset.input) + 1;
+    $el.removeEventListener("input", inputChange);
+    $el.removeEventListener("keydown", pressKey);
+    this.$input = null;
+    this.addInput(id);
+    this.savePrevText($el);
+  }
+  addInput(id) {
+    this.toHtml(".terminal-content", InputTemplate(id));
+    this.focusOnLast();
+    this.disableInputs();
+  }
+  //Inputs
+  setInputs(inputs) {
+    this.inputs = inputs;
+  }
+  disableInputs() {
+    if (this.inputs && this.inputs.length) {
+      const LAST = this.inputs.length - 1;
+      this.inputs.forEach((element, idx) => {
+        if (LAST !== idx) {
+          element.disabled = true;
+        }
+      });
+    }
+  }
+  focusOnLast() {
+    const inputs = this.findAll("#command");
+    root.setInputs(inputs);
+    const last = inputs[inputs.length - 1];
+    last.focus();
+    this.setInput(last);
+  }
+  //Terminal
+  terminalEventClick() {
+    const terminal = this.find(".terminal");
+    terminal.addEventListener("click", (event) => {
+      const target = event.target;
+      if (target.className === "terminal") {
+        this.focusOnLast.bind(this)();
+      }
+    });
+  }
+  //Core
+  init() {
+    this.focusOnLast();
+    this.terminalEventClick();
+    this.disableInputs();
+  }
 }
 
 const root = new Core("#root");
+root.init();
 
 const timer = () => {
   const timerEl = root.find("#timer");
@@ -24,5 +103,28 @@ const timer = () => {
     timer();
   }, 1000);
 };
-
 timer();
+
+//event Handlers
+function inputChange(event) {}
+
+function pressKey(event) {
+  const keys = ["Enter", "Tab"];
+  if (keys.includes(event.key)) {
+    event.preventDefault();
+    root.disableInput(root.$input);
+  }
+}
+const commandInputs = () => {
+  const input = root.find("[data-input='0']");
+  // debugger;
+};
+// commandInputs();
+
+// Templates
+const InputTemplate = (id, value = "") => {
+  return `<div class="terminal-input">
+  <div class="path cyan">admin > </div>
+  <input type="text" id="command" data-input="${id}" value="${value}">
+</div>`;
+};
